d073145cccca5fc9aed662c93197ffc4496c2146
Genna <82814490+genna5623@users.noreply.github.com>
2021-05-05 08:52:47 +0900

    Add files via upload

---
+++ kadai7/kadai7/PinkViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PinkViewController.swift
+//  kadai7
+//
+//  Created by 永野玄 on 2021/05/03.
+//
+
+import UIKit
+
+class PinkViewController: UIViewController {
+    
+    @IBOutlet private weak var firstText: UITextField!
+    
+    @IBOutlet private weak var secondText: UITextField!
+
+    @IBOutlet private weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstText.keyboardType = .numberPad
+        secondText.keyboardType = .numberPad
+    }
+    
+
+    @IBAction func resultbutton(_ sender: Any) {
+        
+        let firstNum = Int(self.firstText.text!) ?? 0
+        let secondNum = Int(self.secondText.text!) ?? 0
+        let result = firstNum+secondNum
+        self.resultLabel.text = String(result)
+        firstText.endEditing(true)
+        secondText.endEditing(true)
+    }
+    
+}
